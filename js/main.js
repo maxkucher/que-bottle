39d0a2a6cb4dfb159fcf1beaefb29f61da3f6373
Maksym Kucher <the.stewk@gmail.com>
2019-07-08 21:37:28 -0700

    Added features section.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
-$('.navigate-to-about').click(() => {
-   $('html,body').animate({
-       scrollTop: $('.section-about')
-           .offset()
-           .top
-   },200)
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.querySelectorAll('.tab-control')
+        .forEach((el) => {
+            el.addEventListener('click', () => {
+                const dataId = el.getAttribute('data-id');
+                const contentPosition = el.getAttribute('data-content-pos');
+                const dataGroup = el.getAttribute('data-group');
+                document.querySelector(`#${contentPosition}`)
+                    .innerHTML = getElementContentById(dataId);
+                document.querySelectorAll(`[data-group="${dataGroup}"]`)
+                    .forEach((el1) => {
+                        el1.classList.remove('active');
+                    });
+                el.classList.add('active');
+
+
+            })
+        });
+
 });
+
+const getElementContentById = (id) => {
+    return document.querySelector(`#${id}`)
+        .innerHTML;
+};
+
