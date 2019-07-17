9bcf1d07ccffea2c7191756e050f1e3c349a5f9a
Maksym Kucher <the.stewk@gmail.com>
2019-07-17 13:59:44 -0700

    - Improved some styling.

--- js/main.js
+++ js/main.js
@@ -1,42 +1,65 @@
+const navigateToGallery = () => {
+    $('html,body').animate({
+        scrollTop: $('.section-gallery')
+            .offset()
+            .top
+    }, 'slow')
+};
+
+const navigateToOrderForm = () => {
+    $('html,body').animate({
+        scrollTop: $('#submit-order-form')
+            .offset()
+            .top
+    }, 'slow')
+};
+
 $(document).ready(() => {
 
-    $('.order-btn').on('click',function (e) {
-       const colorSelected = $(this).attr('data-color');
-       $('#input-color').val(colorSelected);
+
+    $('.order-btn').on('click', function (e) {
+        const colorSelected = $(this).attr('data-color');
+        $('#order-details').html(`Вы выбрали: ${colorSelected} . <br> Стоимость: 250UAH`);
+        $('#input-color').val(colorSelected);
+
     });
 
     $('.navigate-to-order-form').on('click', function (e) {
-        $('html,body').animate({
-            scrollTop: $('#submit-order-form')
-                .offset()
-                .top
-        }, 'slow')
+        navigateToOrderForm();
+    });
+
+    $('.checkout-button').on('click', function (e) {
+        if ($('#input-color').val() === "")
+            navigateToGallery();
+        else navigateToOrderForm();
     });
 
 
     $('.navigate-to-details').on('click', function (e) {
-       $('html,body').animate({
-         scrollTop: $('#details')
-             .offset()
-             .top
-       }, 'slow');
+        $('html,body').animate({
+            scrollTop: $('#details')
+                .offset()
+                .top
+        }, 'slow');
     });
 
 
     $('.navigate-to-gallery').on('click', function (e) {
-        $('html,body').animate({
-            scrollTop: $('.section-gallery')
-                .offset()
-                .top
-        }, 'slow')
+        navigateToGallery();
     });
 
 
     $('#submit-order-form').on('submit', function (e) {
         e.preventDefault();
 
+        if ($('#input-color').val() === "") {
+            alert('Выберите пожалуйста цвет');
+            navigateToGallery();
+            return;
+        }
+
         const name = $('#name').val();
-        const phoneNumber = $('#phone').val()+" .Color: "+$('#input-color').val();
+        const phoneNumber = $('#phone').val() + " .Color: " + $('#input-color').val();
 
 
         $.ajax({
@@ -52,8 +75,8 @@ $(document).ready(() => {
                 $('#name').val("");
                 $('#phone').val("");
                 $('#input-color').val("");
-                alert('Ваша заявка принята. Мы свяжемся с Вам в ближайшее время');
-                console.log(JSON.stringify(data));
+                $('#order-details').text("Выберите пожалуйста цвет");
+                window.location = "ordriscomp.html";
             },
             error(error) {
                 alert('Ошибка отправки заказа. Попробуйте ещё раз.');
